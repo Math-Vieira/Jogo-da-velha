51877a5fe2d9de832777f82adba95114de770357
math-vieira <matheusvieira9153@gmail.com>
2022-03-07 03:53:20 -0300

    jogo finalizado

---
+++ Assets/js/script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+  let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.addEventListener("click", handleClick);
+  });
+});
+
+const handleClick = (e) => {
+  let square = e.target;
+  let position = square.id;
+
+  if (handleMove(position)) {
+    setTimeout(() => {
+      alert("O jogo acabou! O vendedor foi o jogador " + playerTime);
+    }, 100);
+  }
+  updateSquares();
+};
+
+const updateSquares = () => {
+  let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    let position = square.id;
+    let symbol = board[position];
+
+    if (symbol != "") {
+      square.innerHTML = `<div class ='${symbol}'></div>`;
+    }
+  });
+};
+
+function resetGame () {
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach ( element => {
+        element.innerHTML = '';
+    } )
+    board = ["", "", "", "", "", "", "", "", ""];
+    playerTime = 0;
+    symbols = ["o", "x"];
+    gameOver = false;
+}
